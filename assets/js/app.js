56d9147f4df21195690d906f61918cdfcb60cbba
Bozart <bagaricmail@gmail.com>
2025-12-12 19:34:20 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,9 @@
 (() => {
-  console.log("BA-OS loaded");
-
   const mount = document.getElementById("topbar");
   if (!mount) return;
 
   fetch("/bozart-army-web/assets/partials/topbar.html", { cache: "no-cache" })
     .then(r => r.text())
     .then(html => mount.innerHTML = html)
-    .catch(() => console.warn("Topbar failed"));
+    .catch(() => {});
 })();
