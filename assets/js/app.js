248a196699e6e9b420bc5091375fba264c24030d
Bozart <bagaricmail@gmail.com>
2025-12-12 17:22:47 +0100

    Load global nav via JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
-console.log("BA-OS loaded");
+(async () => {
+  console.log("BA-OS loaded");
+
+  const mount = document.createElement("div");
+  document.body.prepend(mount);
+
+  try {
+    const res = await fetch("/assets/partials/nav.html");
+    mount.innerHTML = await res.text();
+  } catch (e) {
+    console.warn("Nav failed to load");
+  }
+})();
